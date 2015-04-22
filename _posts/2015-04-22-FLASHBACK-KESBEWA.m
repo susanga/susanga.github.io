9b5c98d73eb81a231d7252f24a64223870df295b
sdlab100 <sdlab100@gmail.com>
2015-04-22 22:06:34 +0530

    Create 2015-04-22-FLASHBACK-KESBEWA.m

---
+++ _posts/2015-04-22-FLASHBACK-KESBEWA.m
@@ -0,0 +1,84 @@
+---
+layout: post
+title: "FLASHBACK LIVE IN BOMBUWALA 2015"
+pageType: audio
+thumbnail: 140x210/FB-Bombuwala-2015.jpg
+thumbnailSmall: 80x44/Flashback-Bombuwala-2015.jpg
+summary: FLASHBACK LIVE IN BOMBUWALA 
+---
+
+<div class="ab-player" data-boourl="https://audioboom.com/publishing/playlist/v3?autoplay=false&boo_content_type=playlist&data_for_content_type=1275902&image_option=small&link_color=%2358d1eb&player_theme=light&show_title=true&src=https%3A%2F%2Fapi.audioboom.com%2Fplaylists%2F1275902-flashback-live-in-bombuwala-2015" data-boowidth="100%" data-maxheight="285" data-iframestyle="background-color:transparent; display:block; min-width:300px; max-width:700px;" style="background-color:transparent;"></div><script type="text/javascript">(function() { var po = document.createElement("script"); po.type = "text/javascript"; po.async = true; po.src = "https://d15mj6e6qmt1na.cloudfront.net/cdn/embed.js"; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(po, s); })();</script>
+
+{% include album-image.html imgSource ='Flashback-Bombuwala-2015.jpg' %}
+
+### Track list
+
+#### FLASHBACK
+
+- 01 - FLASHBACK INTRO 
+- 02 - CLASSIC NONSTOP 
+- 03 - ANTHIMA SATANE  
+- 04 - DUKA DANNE 
+- 05 - KINGSLY NONSTOP  
+- 06 - AHAN INNAWA NAM 
+- 07 - SELFIE PULA  
+- 08 - HITHA HIRI NEW VERSION 
+- 09 - PISSU VIKARE  
+- 10 - BATHTHAMESI DIL 
+- 11 - LASSANA LOKEKA 
+- 12 - ENGLISH NONSTOP  
+- 13 - SANDA WAGE  
+- 14 - HINDI NONSTOP 
+
+#### SUPRIYA ABESEKARA
+
+- 15 - ADA HAWASATA  
+- 16 - MANDARAME  
+- 17 - RANGANA REGUM 2 IN 1 
+- 18 - S. NONSTOP 
+
+#### CHANDRASENA HETTIARACHCHI
+
+- 19 - MAL MAL 
+- 20 - OYA HINDA 2 IN 1  
+- 21 - OBA HIMI UNA 
+- 22 - ETHA ETHA 
+
+#### CHAMPA KALHARI
+
+- 23 - AJARE  
+- 24 - TERE BINA 
+- 25 - HINDI SONG  
+- 26 - EKA ME 
+- 27 - SUNO CHAMPA
+
+#### CHALINI LAKSHIKA
+
+- 28 - NIHANDA GAMANE 
+- 29 - RON SOYA 
+- 30 - SITHA SENASUMA
+
+#### NIHAL NELSON
+
+- 31 - LASSANATA  
+- 32 - BAYILA 
+- 33 - ORU PADINNA  
+- 34 - GUNE AYYAGE
+
+#### THEEKSHANA ANURADHA
+
+- 35 - PAMA U00 
+- 36 - AYETH HANDANNA  
+- 37 - SAYAM KIRILLI  
+
+#### FLASHBACK
+
+- 38 - END NONSTOP 
+
+
+<a 
+  href="http://www.mediafire.com/download/41atu3102ptcxxy/FLASHBACK_LIVE_IN_BOMBUWALA_2015.rar"
+  class="btn" 
+  target="_blank">
+  DOWNLOAD FULL ALBUM
+</a>
